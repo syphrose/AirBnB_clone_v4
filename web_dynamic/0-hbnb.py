0a6acfeafa91853864349469131a36f29e90644e
TechDanny <kokidanny15@gmail.com>
2023-11-05 05:04:31 -0800

    cash only

---
+++ web_dynamic/0-hbnb.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Starts the flask application"""
+from models import storage
+from uuid import uuid4
+from models.amenity import Amenity
+from models.place import Place
+from models.city import City
+from models.state import State
+from os import environ
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """
+    remove The SQLALchemy session that is current
+    """
+    storage.close()
+
+
+@app.route('/0-hbnb/', strict_slashes=False)
+def hbnb():
+    """
+    HBNB
+    """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    st_ct = []
+
+    for state in states:
+        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
+
+    amenities = storage.all(Amenity).values()
+    amenities = sorted(amenities, key=lambda k: k.name)
+
+    places = storage.all(Place).values()
+    places = sorted(places, key=lambda k: k.name)
+
+    cache_id = str(uuid4())
+
+    return render_template('0-hbnb.html',
+                           states=st_ct,
+                           amenities=amenities,
+                           places=places,
+                           cache_id=cache_id)
+
+    if __name__ == "__main__":
+        app.run(host='0.0.0.0', port=5000)
